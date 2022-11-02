62f9775ec0ce565b777457c2f2931d891ed357df
Z.H <galaxyshih@gmail.com>
2022-11-02 22:14:35 +0800

    init account-app

---
+++ account-app/src/main/java/com/example/accountapp/AccountAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountAppApplication.class, args);
+	}
+
+}
---
+++ account-app/src/test/java/com/example/accountapp/AccountAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
